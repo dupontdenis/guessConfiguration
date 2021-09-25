58b50037db2291ea388c33d0e4735fe2ca0a65b2
Denis Dupont <denis.dupont@univ-evry.fr>
2021-09-25 17:58:57 +0200

    GuessConfJSON

--- conf.js
+++ conf.js
@@ -1,21 +1,19 @@
+const fs = require('fs');
+const path = require('path');
 const { random } = require('./utils');
+
 
 const guess = {
     min: 0,
-    max: 9,
-    get secretnumber() {
-        return this._secretnumber;
-    },
-    set secretnumber(value) {
-        this._secretnumber = value;
-    },
+    max: 10,
     turns: 0,
     limit: 3,
     init() {
         this.secretnumber = parseInt(process.argv[2]) || random(this.min, this.max);
     }
 };
-
+ 
 guess.init();
 
-module.exports = guess;
+ 
+fs.writeFileSync(path.resolve(__dirname, 'guess.json'), JSON.stringify(guess));
--- guess.js
+++ guess.js
@@ -1,4 +1,11 @@
-let { secretnumber, turns, limit } = require('./config');
+const fs = require('fs');
+const path = require('path');
+const conf = require('./conf');
+
+// let rawdata = fs.readFileSync(path.resolve(__dirname, './config.json'));
+let rawdata = fs.readFileSync(path.resolve(__dirname, './guess.json'));
+let { secretnumber, turns, limit} = JSON.parse(rawdata);
+
 
 console.log('Guess a number :');
 process.stdin.on('data', (chunk) => {
